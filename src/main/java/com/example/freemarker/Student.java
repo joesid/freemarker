2ec83979b19d4406aba6d69f819451c59b7ade0c
Joe <joesidious@gmail.com>
2023-05-01 20:53:37 +0100

    Creation of Student Class

---
+++ src/main/java/com/example/freemarker/Student.java
@@ -0,0 +1,14 @@
+package com.example.freemarker;
+
+public class Student {
+    private Integer id;
+    private String studentName;
+    private double grade;
+
+
+    public Student(Integer id, String studentName, double grade){
+        Id = id;
+        this.studentName = studentName;
+        this.grade = grade;
+    }
+}
